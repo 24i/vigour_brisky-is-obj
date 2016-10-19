f9dbe7057aeb001a49f078094657f3b9532bf153
Jim de Beer <jim@vigour.io>
2016-10-19 15:56:14 +0200

    added brisky-base and yarn

--- test/index.js
+++ test/index.js
@@ -1,19 +1,18 @@
 'use strict'
+const test = require('tape')
+const base = require('brisky-base')
+const isObj = require('../')
 
-var test = require('tape')
-var Base = require('vigour-base')
-var isObj = require('../')
-
-var testCases = [
+const testCases = [
   [{}, true],
   [{ a: 'a' }, true],
-  [new Base({}), false],
+  [base({}), false],
   [[], true],
   [['a'], true],
   [function () {}, false]
 ]
 
-test('isObj', function (t) {
+test('isObj', t => {
   t.plan(testCases.length)
   testCases.forEach(function (item) {
     t.equals(isObj(item[0]), item[1], 'isObj(' + JSON.stringify(item[0]) + ') === ' + item[1])
